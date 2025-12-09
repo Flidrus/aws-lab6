9cd25876403b04c12f8141784bf5798098cb9ce1
Flidrus <gomelskiy2005@gmail.com>
2025-12-09 12:41:26 +0200

    initial commit

---
+++ test_app.py
@@ -0,0 +1,18 @@
+import unittest
+import app as tested_app
+import json
+
+
+class FlaskAppTests(unittest.TestCase):
+
+    def setUp(self):
+        self.app = tested_app.app.test_client()
+
+    def test_get_hello_endpoint(self):
+        r = self.app.get('/')
+        self.assertEqual(r.status, '200 OK')
+        self.assertEqual(r.data, b'Hello world from app! It is Pipeline testinig.')
+
+
+if __name__ == '__main__':
+    unittest.main()
