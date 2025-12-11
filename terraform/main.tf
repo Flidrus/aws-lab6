6e6312739ec005397989ea3101a6cca90c07e6ab
Flidrus <gomelskiy2005@gmail.com>
2025-12-11 17:29:20 +0200

    Added Lightsail

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,6 +20,10 @@ provider "aws" {
   region = "eu-central-1"
 }
 
+variable "REPOSITORY_URI" {
+  type = string
+}
+
 resource "aws_security_group" "web_app" {
   name        = "web_app"
   description = "security group"
@@ -63,4 +67,48 @@ resource "aws_instance" "webapp_instance" {
 output "instance_public_ip" {
   value     = aws_instance.webapp_instance.public_ip
   sensitive = true
+}
+
+resource "aws_lightsail_container_service" "flask_application" {
+  name  = "flask-app"
+  power = "nano"
+  scale = 1
+
+  private_registry_access {
+    ecr_image_puller_role {
+      is_active = true
+    }
+  }
+
+  tags = {
+    version = "1.0.0"
+  }
+}
+
+resource "aws_lightsail_container_service_deployment_version" "flask_app_deployment" {
+  container {
+    container_name = "flask-application"
+    image          = "${var.REPOSITORY_URI}:latest"
+  }
+
+  ports = {
+    80 = "HTTP"
+  }
+
+  public_endpoint {
+    container_name = "flask-application"
+    container_port = 8080
+
+    health_check {
+      healthy_threshold   = 2
+      unhealthy_threshold = 2
+      timeout_seconds     = 2
+      interval_seconds    = 10
+      path                = "/"
+      success_codes       = "200-499"
+    }
+  }
+  
+  service_name = aws_lightsail_container_service.flask_application.name
+  
 }
