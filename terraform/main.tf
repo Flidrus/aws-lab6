eaae45e3e4e33db3ac10ff9d874484ba3cd8ea81
Flidrus <gomelskiy2005@gmail.com>
2025-12-28 20:19:47 +0200

    Configured application on port 5050

--- terraform/main.tf
+++ terraform/main.tf
@@ -47,6 +47,13 @@ resource "aws_security_group" "web_app" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 5050
+    to_port     = 5050
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 65535
